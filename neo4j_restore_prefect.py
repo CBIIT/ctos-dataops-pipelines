1f312fe18362807a012cea2a141d7de5cfb47412
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2024-05-01 11:47:47 -0400

    Update neo4j_restore_prefect.py

--- neo4j_restore_prefect.py
+++ neo4j_restore_prefect.py
@@ -33,7 +33,7 @@ def neo4j_restore_prefect(
     neo4j_ip = secret[NEO4J_IP]
     neo4j_user = secret_ssh[NEO4J_USER]
     neo4j_key = secret_ssh[NEO4J_KEY]
-    neo4j_restore(neo4j_ip, neo4j_user, neo4j_key, s3_bucket, s3_file_key)
+    #neo4j_restore(neo4j_ip, neo4j_user, neo4j_key, s3_bucket, s3_file_key)
 
     neo4j_summary_user = secret[NEO4J_USER]
     neo4j_summary_password = secret[NEO4J_PASSWORD]

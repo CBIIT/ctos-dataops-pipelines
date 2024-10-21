ee155558a782de165b73e4e542991e07aaabe144
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2024-10-21 17:12:19 -0400

    Update memgraph_export.py

--- memgraph_export.py
+++ memgraph_export.py
@@ -1,12 +1,15 @@
 import subprocess
 import os
+import sys
 from bento.common.utils import get_time_stamp
 from bento.common.s3 import upload_log_file
 
 
 
 def upload_s3(s3_prefix, s3_bucket, file_key, log):
+    
     dest = os.path.join(f"s3://{s3_bucket}", s3_prefix)
+    log.info(dest)
     upload_log_file(dest, file_key)
     log.info(f'Uploading memgraph export file {os.path.basename(file_key)} succeeded!')
 
@@ -24,4 +27,5 @@ def memgraph_export(memgraph_host, memgraph_port, memgraph_username, memgraph_pa
         upload_s3(s3_prefix, s3_bucket, export_file_key, log)
     except Exception as e:
         log.error(e)
+        sys.exit(1)
 

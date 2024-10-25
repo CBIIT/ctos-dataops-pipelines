8e67688616584873517a133706c6e73e6ae93f6f
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2024-10-25 13:57:47 -0400

    Update to make the "where" section will have an option to affect children nodes

--- neo4j_data_tsv_exporter.py
+++ neo4j_data_tsv_exporter.py
@@ -21,6 +21,7 @@ if LOG_PREFIX not in os.environ:
     os.environ[LOG_PREFIX] = 'Neo4j_TSV_Export'
     os.environ[APP_NAME] = 'Neo4j_TSV_Export'
 
+
 def write_to_tsv(output_key, node, results, query_parent_dict, log):
     with open(output_key, "w", newline="") as csvfile:
         fieldnames = set()
@@ -50,31 +51,42 @@ def write_to_tsv(output_key, node, results, query_parent_dict, log):
     log.info(f"Data has been written to {output_key}")
 
 def create_query(config, node, schema):
-    query = f"MATCH (n:{node})"
-    if WHERE in config.keys():
-        if isinstance(config[WHERE], dict):
-            for w in config[WHERE]:
-                prop_node = w.split(".")[0]
-                prop = w.split(".")[1]
-                pv = config[WHERE][w]
-                if node == prop_node:
-                    if WHERE not in query:
-                        query = query + f" WHERE n.{prop} IN {str(pv)}"
-                    else:
-                        query = query + f" AND n.{prop} in {str(pv)}"
-    query_parent_dict = {}
+    #query = f"MATCH (n:{node})"
     parent_list = check_parents(node, schema)
+    query_match = f"MATCH (n:{node})"
+    query_where = ""
+    query_optional = ""
+    query_return = " Return n"
+    query_parent_dict = {}
     if len(parent_list) > 0:
         query_parent_count = 0
         for parent in parent_list:
             query_parent_count += 1
             query_parent_value = "p" + str(query_parent_count)
-            query = query + f" OPTIONAL MATCH (n)-->({query_parent_value}:{parent})"
+            query_optional = query_optional + f" OPTIONAL MATCH (n)-->({query_parent_value}:{parent})"
             query_parent_dict[query_parent_value] = {parent: check_parents_id(parent, schema)}
-    query = query + " Return n"
     if len(query_parent_dict.keys())>0:
         for qpv in query_parent_dict.keys():
-            query = query + f",{qpv}"
+            query_return = query_return + f",{qpv}"
+    if WHERE in config.keys():
+        if isinstance(config[WHERE], dict):
+            for w in config[WHERE]:
+                prop_node = w.split(".")[0]
+                prop = w.split(".")[1]
+                pv = config[WHERE][w].get("values")
+                where_connected = config[WHERE][w].get("where_connected")
+                if node == prop_node:
+                    if "WHERE" not in query_where:
+                        query_where = query_where + f" WHERE n.{prop} IN {str(pv)}"
+                    else:
+                        query_where = query_where + f" AND n.{prop} in {str(pv)}"
+                elif prop_node in parent_list and where_connected:
+                    query_match = query_match + f" MATCH (n)-->({prop_node}:{prop_node})"
+                    if "WHERE" not in query_where:
+                        query_where = query_where + f" WHERE {prop_node}.{prop} IN {str(pv)}"
+                    else:
+                        query_where = query_where + f" AND {prop_node}.{prop} IN {str(pv)}"
+    query = query_match + query_where + query_optional + query_return
     return query, query_parent_dict
 
 def get_schema(config, log):
@@ -106,7 +118,7 @@ def check_parents_id(node, schema):
 
 def main():
     log = get_logger('Neo4j Data TSV Exporting')
-    with open("config/neo4j_data_tsv_exporter_config_example.yaml") as f:
+    with open("config/neo4j_data_tsv_exporter_config_example_test.yaml") as f:
         config = yaml.safe_load(f)
     schema = get_schema(config, log)
 
@@ -118,6 +130,7 @@ def main():
         )
     for node in config["nodes"]:
         query, query_parent_dict = create_query(config, node, schema)
+        print(query)
         with driver.session() as session:
             session = driver.session()
             results = session.run(query)

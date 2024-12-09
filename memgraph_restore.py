56f4359c4dc5a7cf0cc8d692e32e80d1d3669a0b
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2024-12-09 15:27:31 -0500

    Update memgraph_restore.py

--- memgraph_restore.py
+++ memgraph_restore.py
@@ -7,8 +7,6 @@ def memgraph_restore(memgraph_host, memgraph_port, memgraph_username, memgraph_p
     try:
         restore_file_key = os.path.join(tmp_folder, export_filename)
         s3_file_key = os.path.join(s3_prefix, export_filename)
-        log.info(s3_file_key)
-        log.info(restore_file_key)
         download_succeeded = downlaod_s3(s3_bucket, s3_file_key, log, restore_file_key)
         if download_succeeded:
             mgconsole_string = f" | mgconsole --host {memgraph_host} --port {memgraph_port} --username {memgraph_username} --password {memgraph_password}"
@@ -17,8 +15,8 @@ def memgraph_restore(memgraph_host, memgraph_port, memgraph_username, memgraph_p
                 "-c",
                 f'echo "MATCH (n) DETACH DELETE n;"' + mgconsole_string
                 ]
-            command_restore = [f'cat {restore_file_key}' + mgconsole_string]
-            commands = [f"ls {tmp_folder}", command_delete, command_restore]
+            #command_restore = [f'cat {restore_file_key}' + mgconsole_string]
+            commands = [command_delete]
             for command in commands:
                 result = subprocess.run(command, capture_output=True, text=True)
                 log.info(result)

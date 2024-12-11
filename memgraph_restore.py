8cfb5e9cd1d8d2433e80ec9139585bd9fe0ac93e
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2024-12-11 15:15:21 -0500

    error information update testing

--- memgraph_restore.py
+++ memgraph_restore.py
@@ -2,14 +2,13 @@ import subprocess
 import os
 import sys
 from neo4j_restore import downlaod_s3
+from memgraph_export import remove_information
 
 def memgraph_restore(memgraph_host, memgraph_port, memgraph_username, memgraph_password, tmp_folder, s3_bucket, s3_prefix, export_filename, log):
     try:
         restore_file_key = os.path.join(tmp_folder, export_filename)
         s3_file_key = os.path.join(s3_prefix, export_filename)
         download_succeeded = downlaod_s3(s3_bucket, s3_file_key, log, restore_file_key)
-        with open(restore_file_key, 'r') as file:
-            log.info(file)
         if download_succeeded:
             mgconsole_string = f"mgconsole --host {memgraph_host} --port {memgraph_port} --username {memgraph_username} --password {memgraph_password}"
             command_delete = [
@@ -18,8 +17,8 @@ def memgraph_restore(memgraph_host, memgraph_port, memgraph_username, memgraph_p
                 f'echo "MATCH (n) DETACH DELETE n;"' + " | " + mgconsole_string
                 ]
             command_restore = [
-                "sh",
-                "-c",
+                #"sh",
+                #"-c",
                 mgconsole_string + f" < {restore_file_key}"]
             commands = [command_delete, command_restore]
             for command in commands:
@@ -27,6 +26,7 @@ def memgraph_restore(memgraph_host, memgraph_port, memgraph_username, memgraph_p
                 #log.info(result)
             log.info(f"Successsfuly restore the Memgraph data from {export_filename}")
     except Exception as e:
-        log.error(e)
+        updated_error_message = remove_information(str(e), [memgraph_password, memgraph_username, memgraph_port])
+        log.error(updated_error_message)
         sys.exit(1)
 

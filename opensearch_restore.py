35ed196158dae49afddcabe550b437502ce48ad1
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2026-02-19 13:27:39 -0500

    Remove --rolearn CLI option
    Remove the --rolearn argparse option and the corresponding rolearn entry in argList within opensearch_restore.py. This simplifies the CLI by eliminating an unused/deprecated role ARN parameter; no other behavior in the script was changed.

--- opensearch_restore.py
+++ opensearch_restore.py
@@ -12,7 +12,6 @@ def getArgs():
   parser.add_argument("--s3bucket", type=str, help="s3 bucket")
   parser.add_argument("--snapshot", type=str, help="opensearch snapshot value")
   parser.add_argument("--indices", type=str, help="indices", nargs='?', const='')
-  parser.add_argument("--rolearn", type=str, help="role arn - typically power user role")
   parser.add_argument("--basepath", type=str, help="basepath", nargs='?', const='')
   parser.add_argument("--region", type=str, help="region")
   args = parser.parse_args()
@@ -23,7 +22,6 @@ def getArgs():
   argList['s3bucket'] = args.s3bucket
   argList['snapshot'] = args.snapshot 
   argList['indices'] = args.indices
-  argList['rolearn'] = args.rolearn
   argList['region'] = args.region
 
   basepath = args.basepath

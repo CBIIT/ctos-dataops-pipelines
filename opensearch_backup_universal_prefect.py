4633c0ae3af5de81a8c65302698305b2994ec56e
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2026-04-20 13:29:27 -0400

    add universal memgraph backup

---
+++ opensearch_backup_universal_prefect.py
@@ -0,0 +1,60 @@
+from prefect import flow
+from bento.common.utils import get_logger, LOG_PREFIX, APP_NAME
+from bento.common.secret_manager import get_secret
+import os
+import prefect.variables as Variable
+from opensearch_backup import opensearch_backup
+import boto3
+
+SUMARY_SECRET = "memgraph_summary_secret"
+ES_HOST = "es_host"
+#PROJECT_NAME  = "gen"
+REGION = "us-east-1"
+ENVIRONMENT = "env"
+
+if LOG_PREFIX not in os.environ:
+    os.environ[LOG_PREFIX] = 'OpenSearch Backup'
+    os.environ[APP_NAME] = 'OpenSearch Backup'
+
+
+def get_aws_account_id(log):
+    try:
+        sts_client = boto3.client("sts")
+        response = sts_client.get_caller_identity()
+        account_id = response["Account"]
+        return account_id
+    except Exception as e:
+        log.info(f"Error getting AWS account ID: {e}")
+        return None
+
+
+
+@flow(name="OpenSearch backup", log_prints=True)
+def opensearch_backup_prefect(
+    snapshot_name,
+    secrect_prefect_variable,
+    aws_role_env,
+    opensearch_repo,
+    s3_bucket,
+    indices
+):
+    log = get_logger('OpenSearch Backup')
+    opensearch_secret = Variable.get(secrect_prefect_variable)
+    secret = get_secret(opensearch_secret)
+    aws_account_id = get_aws_account_id(log)
+    role_arn = f"arn:aws:iam::{aws_account_id}:role/power-user-crdc-{aws_role_env}-cds-opensearch-snapshot"
+    argList = {
+        'oshost': "https://" + secret[ES_HOST] + "/",
+        'repo': opensearch_repo,
+        's3bucket': s3_bucket,
+        'snapshot': snapshot_name,
+        'indices': indices,
+        'rolearn': role_arn,
+        'region': REGION,
+        'basepath': snapshot_name
+    }
+    opensearch_backup(argList)
+
+if __name__ == "__main__":
+    # create your first deployment
+   opensearch_backup_prefect.serve(name="opensearch_backup")

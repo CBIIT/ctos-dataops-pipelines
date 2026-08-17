1755b28bddd793b3c6db1efe38c9b5c98b1636da
Joon Lee <joon.lee2@nih.gov>
2026-08-17 10:30:38 -0400

    Trying to fix parameters

--- data_asset_generation_prefect.py
+++ data_asset_generation_prefect.py
@@ -1,11 +1,8 @@
 from prefect import flow
-from neo4j_dump_prefect import neo4j_dump_prefect
-from neo4j_summary_prefect import neo4j_secret_summary_prefect
-from data_model_archiving_prefect import data_model_archiving_prefect
 from datetime import datetime
 from github_refs import get_github_refs
 import prefect.variables as Variable
-from typing import Literal
+from typing import Literal, Optional
 import yaml
 
 config_file = "config/prefect_drop_down_config.yaml"
@@ -19,9 +16,15 @@ data_model_repo_url_default = (
     prefect_config.get("data_model_repo_url")
     or "https://github.com/CBIIT/ins-model"
 )
-data_model_version_choices = Literal[
-    tuple(get_github_refs(data_model_repo_url_default, include_tags=True))
-]
+data_model_versions = get_github_refs(
+    data_model_repo_url_default,
+    include_tags=True,
+)
+data_model_version_choices = (
+    Literal[tuple(data_model_versions)]
+    if data_model_versions
+    else str
+)
 SUMARY_SECRET = "neo4j_summary_secret"
 DUMP_SECRET = "neo4j_ssh_secret"
 
@@ -29,12 +32,16 @@ DUMP_SECRET = "neo4j_ssh_secret"
 def data_asset_generation_prefect(
         environment: environment_choices, # type: ignore
         data_model_version: data_model_version_choices, # type: ignore
-        s3_bucket,
-        s3_folder="dump_files",
-        neo4j_summary_file_name=None,
-        neo4j_dump_file_name=None,
-        data_model_repo_url=data_model_repo_url_default,
+        s3_bucket: str,
+        s3_folder: str="dump_files",
+        neo4j_summary_file_name: Optional[str]=None,
+        neo4j_dump_file_name: Optional[str]=None,
+        data_model_repo_url: str=data_model_repo_url_default,
     ):
+    from data_model_archiving_prefect import data_model_archiving_prefect
+    from neo4j_dump_prefect import neo4j_dump_prefect
+    from neo4j_summary_prefect import neo4j_secret_summary_prefect
+
     neo4j_summary_secret = Variable.get(config[environment][SUMARY_SECRET])
     neo4j_dump_secret = Variable.get(config[environment][DUMP_SECRET])
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M")

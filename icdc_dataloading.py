801c5df9f26fdb335690e95e09a91e43419a13a3
Eric Miller <ermiller@alumni.cmu.edu>
2026-08-21 16:54:38 -0400

    chore: remove unused import

--- icdc_dataloading.py
+++ icdc_dataloading.py
@@ -6,10 +6,10 @@ Ported from the legacy icdc-dataloader repo's loader.py/main(), trimmed to a
 plain-dict entry point (icdc_dataloading(argList)) for use by the Prefect
 wrapper, since Prefect flow parameters replace the CLI/BentoConfig layer.
 """
+
 import argparse
 import glob
 import os
-import sys
 import zipfile
 
 from neo4j import GraphDatabase
@@ -32,13 +32,13 @@ from bento.common.utils import (
 from bento.common.s3 import S3Bucket, upload_log_file
 
 if LOG_PREFIX not in os.environ:
-    os.environ[LOG_PREFIX] = 'ICDC_Data_Loader'
-    os.environ[APP_NAME] = 'ICDC_Data_Loader'
+    os.environ[LOG_PREFIX] = "ICDC_Data_Loader"
+    os.environ[APP_NAME] = "ICDC_Data_Loader"
 
 DEFAULT_MAX_VIOLATIONS = 1000000
 DEFAULT_TEMP_FOLDER = "tmp"
 
-logger = get_logger('ICDC Data Loader')
+logger = get_logger("ICDC Data Loader")
 
 
 def icdc_dataloading(argList: dict):
@@ -71,38 +71,42 @@ def icdc_dataloading(argList: dict):
     """
     log_file = get_log_file()
 
-    dataset = argList.get('dataset') or 'data'
-    s3_bucket = argList.get('s3_bucket')
-    s3_folder = argList.get('s3_folder')
-    schema_files = argList['schema_files']
-    prop_file = argList['prop_file']
-    neo4j_uri = removeTrailingSlash(argList.get('neo4j_uri') or 'bolt://localhost:7687')
-    neo4j_user = argList.get('neo4j_user') or 'neo4j'
-    neo4j_password = argList['neo4j_password']
-    mode = argList.get('mode') or UPSERT_MODE
-    cheat_mode = argList.get('cheat_mode', False)
-    dry_run = argList.get('dry_run', False)
-    wipe_db = argList.get('wipe_db', False)
-    split_transactions = argList.get('split_transactions', False)
-    no_backup = argList.get('no_backup', True)
-    backup_folder = argList.get('backup_folder')
-    max_violations = argList.get('max_violations') or DEFAULT_MAX_VIOLATIONS
-    temp_folder = argList.get('temp_folder') or DEFAULT_TEMP_FOLDER
-    verbose = argList.get('verbose', False)
-    plugins = argList.get('plugins') or []
+    dataset = argList.get("dataset") or "data"
+    s3_bucket = argList.get("s3_bucket")
+    s3_folder = argList.get("s3_folder")
+    schema_files = argList["schema_files"]
+    prop_file = argList["prop_file"]
+    neo4j_uri = removeTrailingSlash(argList.get("neo4j_uri") or "bolt://localhost:7687")
+    neo4j_user = argList.get("neo4j_user") or "neo4j"
+    neo4j_password = argList["neo4j_password"]
+    mode = argList.get("mode") or UPSERT_MODE
+    cheat_mode = argList.get("cheat_mode", False)
+    dry_run = argList.get("dry_run", False)
+    wipe_db = argList.get("wipe_db", False)
+    split_transactions = argList.get("split_transactions", False)
+    no_backup = argList.get("no_backup", True)
+    backup_folder = argList.get("backup_folder")
+    max_violations = argList.get("max_violations") or DEFAULT_MAX_VIOLATIONS
+    temp_folder = argList.get("temp_folder") or DEFAULT_TEMP_FOLDER
+    verbose = argList.get("verbose", False)
+    plugins = argList.get("plugins") or []
 
     if plugins:
-        raise NotImplementedError('Plugin support has not been ported yet; pass an empty plugins list.')
+        raise NotImplementedError(
+            "Plugin support has not been ported yet; pass an empty plugins list."
+        )
 
     if split_transactions and no_backup:
-        raise Exception('split_transactions and no_backup cannot both be enabled, a backup is required '
-                         'when running in split transactions mode')
+        raise Exception(
+            "split_transactions and no_backup cannot both be enabled, a backup is required "
+            "when running in split transactions mode"
+        )
     if not backup_folder and not no_backup:
-        raise Exception('backup_folder is required unless no_backup is True')
+        raise Exception("backup_folder is required unless no_backup is True")
 
-    upload_log_dir = argList.get('upload_log_dir')
+    upload_log_dir = argList.get("upload_log_dir")
     if not upload_log_dir and s3_bucket and s3_folder:
-        upload_log_dir = f's3://{s3_bucket}/{s3_folder}/logs'
+        upload_log_dir = f"s3://{s3_bucket}/{s3_folder}/logs"
 
     print_config(logger, argList)
 
@@ -113,13 +117,15 @@ def icdc_dataloading(argList: dict):
         if not os.path.exists(dataset):
             os.makedirs(dataset)
         else:
-            exist_files = glob.glob('{}/*.txt'.format(dataset))
+            exist_files = glob.glob("{}/*.txt".format(dataset))
             if len(exist_files) > 0:
-                raise Exception(f'Folder: "{dataset}" is not empty, please empty it first')
+                raise Exception(
+                    f'Folder: "{dataset}" is not empty, please empty it first'
+                )
         if not s3_bucket:
-            raise Exception('s3_bucket is required when s3_folder is set')
+            raise Exception("s3_bucket is required when s3_folder is set")
         bucket = S3Bucket(s3_bucket)
-        logger.info(f'Loading data from s3://{s3_bucket}/{s3_folder}')
+        logger.info(f"Loading data from s3://{s3_bucket}/{s3_folder}")
         if not bucket.download_files_in_folder(s3_folder, dataset):
             raise Exception(f'Download files from S3 bucket "{s3_bucket}" failed!')
 
@@ -127,8 +133,8 @@ def icdc_dataloading(argList: dict):
     load_result = None
     zip_file_key = None
     try:
-        txt_files = glob.glob('{}/*.txt'.format(dataset))
-        tsv_files = glob.glob('{}/*.tsv'.format(dataset))
+        txt_files = glob.glob("{}/*.txt".format(dataset))
+        tsv_files = glob.glob("{}/*.tsv".format(dataset))
         file_list = txt_files + tsv_files
         if file_list:
             prop_path = os.path.join(dataset, prop_file)
@@ -144,26 +150,49 @@ def icdc_dataloading(argList: dict):
             loader = DataLoader(driver, schema, plugins)
 
             load_result = loader.load(
-                file_list, cheat_mode, dry_run, mode, wipe_db, max_violations, temp_folder, verbose,
-                split=split_transactions, no_backup=no_backup, backup_folder=backup_folder, neo4j_uri=neo4j_uri,
+                file_list,
+                cheat_mode,
+                dry_run,
+                mode,
+                wipe_db,
+                max_violations,
+                temp_folder,
+                verbose,
+                split=split_transactions,
+                no_backup=no_backup,
+                backup_folder=backup_folder,
+                neo4j_uri=neo4j_uri,
             )
 
             if load_result is False:
                 if loader.validation_result_file_key:
-                    zip_file_key = loader.validation_result_file_key.replace(".xlsx", ".zip")
-                    with zipfile.ZipFile(zip_file_key, 'w') as zipf:
-                        zipf.write(loader.validation_result_file_key, os.path.basename(loader.validation_result_file_key))
+                    zip_file_key = loader.validation_result_file_key.replace(
+                        ".xlsx", ".zip"
+                    )
+                    with zipfile.ZipFile(zip_file_key, "w") as zipf:
+                        zipf.write(
+                            loader.validation_result_file_key,
+                            os.path.basename(loader.validation_result_file_key),
+                        )
                         zipf.write(log_file, os.path.basename(log_file))
-                    logger.error('Data loading failed, validation result zip file was created at {}'.format(zip_file_key))
+                    logger.error(
+                        "Data loading failed, validation result zip file was created at {}".format(
+                            zip_file_key
+                        )
+                    )
                 else:
-                    logger.error('Data loading failed')
+                    logger.error("Data loading failed")
             else:
                 zip_file_key = log_file.replace(".log", ".zip")
-                with zipfile.ZipFile(zip_file_key, 'w') as zipf:
+                with zipfile.ZipFile(zip_file_key, "w") as zipf:
                     zipf.write(log_file, os.path.basename(log_file))
-                logger.info('Data loading succeeded, zip file was created at {}'.format(zip_file_key))
+                logger.info(
+                    "Data loading succeeded, zip file was created at {}".format(
+                        zip_file_key
+                    )
+                )
         else:
-            logger.info('No files to load.')
+            logger.info("No files to load.")
 
     except ServiceUnavailable:
         logger.critical('Neo4j service not available at: "{}"'.format(neo4j_uri))
@@ -178,26 +207,31 @@ def icdc_dataloading(argList: dict):
     if upload_log_dir and zip_file_key:
         try:
             upload_log_file(upload_log_dir, zip_file_key)
-            logger.info(f'Uploading log/validation zip file {zip_file_key} succeeded!')
+            logger.info(f"Uploading log/validation zip file {zip_file_key} succeeded!")
         except Exception as e:
             logger.debug(e)
-            logger.exception('Uploading log file failed! Check debug log for detailed information')
+            logger.exception(
+                "Uploading log file failed! Check debug log for detailed information"
+            )
 
     if load_result is False:
-        raise Exception('Data loading failed; see logs for details')
+        raise Exception("Data loading failed; see logs for details")
 
     return load_result
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Load TSV files from S3 into Neo4j')
-    parser.add_argument('config_file', type=argparse.FileType('r'), help='Loader configuration YAML')
+    parser = argparse.ArgumentParser(description="Load TSV files from S3 into Neo4j")
+    parser.add_argument(
+        "config_file", type=argparse.FileType("r"), help="Loader configuration YAML"
+    )
     args = parser.parse_args()
 
     import yaml
-    config = yaml.safe_load(args.config_file)['Config']
+
+    config = yaml.safe_load(args.config_file)["Config"]
     icdc_dataloading(config)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

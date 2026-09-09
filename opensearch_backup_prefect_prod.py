37c43d99fbdc4900adff40d864a51566de1f714d
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2026-09-09 10:35:48 -0400

    Add prod Prefect flows for OpenSearch ops
    Introduce new production Prefect entrypoints for OpenSearch backup and restore. Both flows load environment choices from the Memgraph dropdown config, resolve OpenSearch secrets and IAM role ARNs from Prefect variables, build the operation arguments, and invoke the existing backup/restore utilities with snapshot, indices, and S3 bucket inputs.

---
+++ opensearch_backup_prefect_prod.py
@@ -0,0 +1,52 @@
+from prefect import flow
+from typing import Literal
+import yaml
+from bento.common.utils import get_logger, LOG_PREFIX, APP_NAME
+from bento.common.secret_manager import get_secret
+import os
+import prefect.variables as Variable
+from opensearch_backup import opensearch_backup
+
+SUMMARY_SECRET = "memgraph_summary_secret"
+ES_HOST = "es_host"
+PROJECT_NAME  = "ctdc"
+REGION = "us-east-1"
+ENVIRONMENT = "env"
+
+if LOG_PREFIX not in os.environ:
+    os.environ[LOG_PREFIX] = 'OpenSearch Backup'
+    os.environ[APP_NAME] = 'OpenSearch Backup'
+
+config_file = "config/prefect_drop_down_config_memgraph.yaml"
+with open(config_file, 'r') as file:
+    config = yaml.safe_load(file)
+environment_choices = Literal[tuple(list(config.keys()))]
+
+@flow(name="OpenSearch backup", log_prints=True)
+def opensearch_backup_prefect(
+    environment: environment_choices, # type: ignore
+    snapshot_name,
+    indices,
+    s3_bucket
+):
+    log = get_logger('OpenSearch Backup')
+    opensearch_secret = Variable.get(config[environment][SUMMARY_SECRET])
+    secret = get_secret(opensearch_secret)
+    role_arn = Variable.get("ctdc_role_arn_prod")
+    os_role_arn = Variable.get("ctdc_os_role_arn_prod")
+    argList = {
+        'oshost': "https://" + secret[ES_HOST] + "/",
+        'repo': PROJECT_NAME,
+        's3bucket': s3_bucket,
+        'snapshot': snapshot_name,
+        'indices': indices,
+        'rolearn': role_arn,
+        'osrolearn': os_role_arn,
+        'region': REGION,
+        'basepath': snapshot_name
+    }
+    opensearch_backup(argList)
+
+if __name__ == "__main__":
+    # create your first deployment
+   opensearch_backup_prefect.serve(name="opensearch_backup")
---
+++ opensearch_restore_prefect_prod.py
@@ -0,0 +1,52 @@
+from prefect import flow
+from typing import Literal
+import yaml
+from bento.common.utils import get_logger, LOG_PREFIX, APP_NAME
+from bento.common.secret_manager import get_secret
+import os
+import prefect.variables as Variable
+from opensearch_restore import opensearch_restore
+
+
+SUMMARY_SECRET = "memgraph_summary_secret"
+ES_HOST = "es_host"
+PROJECT_NAME  = "ctdc"
+REGION = "us-east-1"
+ENVIRONMENT = "env"
+
+if LOG_PREFIX not in os.environ:
+    os.environ[LOG_PREFIX] = 'OpenSearch Restore'
+    os.environ[APP_NAME] = 'OpenSearch Restore'
+
+config_file = "config/prefect_drop_down_config_memgraph.yaml"
+with open(config_file, 'r') as file:
+    config = yaml.safe_load(file)
+environment_choices = Literal[tuple(list(config.keys()))]
+
+@flow(name="OpenSearch restore", log_prints=True)
+def opensearch_restore_prefect(
+    environment: environment_choices, # type: ignore
+    snapshot_name,
+    indices,
+    s3_bucket
+):
+    log = get_logger('OpenSearch Restore')
+    opensearch_secret = Variable.get(config[environment][SUMMARY_SECRET])
+    secret = get_secret(opensearch_secret)
+    role_arn = Variable.get("ctdc_role_arn_prod")
+    os_role_arn = Variable.get("ctdc_os_role_arn_prod")
+    argList = {
+        'oshost': "https://" + secret[ES_HOST] + "/",
+        'repo': PROJECT_NAME,
+        's3bucket': s3_bucket,
+        'snapshot': snapshot_name,
+        'indices': indices,
+        'rolearn': role_arn,
+        'osrolearn': os_role_arn,
+        'region': REGION,
+        'basepath': snapshot_name
+    }
+    opensearch_restore(argList)
+
+if __name__ == "__main__":
+   opensearch_restore_prefect.serve(name="opensearch_restore")
